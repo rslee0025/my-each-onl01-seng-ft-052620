7eef64116f36f6b7a0114a6aa3396c6e42e08ff6
Rachel S. Lee <rslee0025@gmail.com>
2020-05-31 18:59:05 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -13,14 +13,14 @@
    
  counter = 0 
  
- while counter < array.length           #during each step of the
+ while counter < array.length           
     yield array[counter]
     counter += 1 
   end
 end  
   
-
- 
+#during each step of the while loop, counter equals a given index number of our array. The first loop will be 1, the second will be 2, etc. 
+ #the [ ] is used to grab the value of each successive index element as we proceed through the while loop, yielding each to a blocl.
  
  
  

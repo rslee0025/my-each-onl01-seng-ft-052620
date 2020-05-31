d3fb7a3d347c2f3a56789e4dd4d99f2d5866123e
Rachel S. Lee <rslee0025@gmail.com>
2020-05-31 19:01:09 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,17 @@
-def my_each # put argument(s) here
-  # code here
-end
+ def my_each(array)
+   
+ counter = 0 
+ 
+ while counter < array.length           
+    yield array[counter]
+    counter += 1 
+  end
+  return array
+end  
+  
+#during each step of the while loop, counter equals a given index number of our array. The first loop will be 1, the second will be 2, etc. 
+ #the [ ] is used to grab the value of each successive index element as we proceed through the while loop, yielding each to a block.
+ 
+
+ 
+   

a153dbda0b5323a0425054f3ee98bed785af9b1f
Rachel S. Lee <rslee0025@gmail.com>
2020-05-31 18:56:55 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,18 +1,24 @@
-require 'pry'
+#require 'pry'
 
-def my_each(array)
-    array.map do |word|
-    end  
-  return array
-  binding.pry
-end  
+#def my_each(array)
+#    array.map do |word|
+#    end  
+#  return array
+#  binding.pry
+#end  
  
 
  
  def my_each(array)
    
+ counter = 0 
  
- 
+ while counter < array.length           #during each step of the
+    yield array[counter]
+    counter += 1 
+  end
+end  
+  
 
  
  

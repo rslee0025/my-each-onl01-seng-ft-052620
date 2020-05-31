e8445d0c1a90e1091407e905d39dda3d17e4f52e
Rachel S. Lee <rslee0025@gmail.com>
2020-05-31 19:00:19 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -22,28 +22,6 @@ end
 #during each step of the while loop, counter equals a given index number of our array. The first loop will be 1, the second will be 2, etc. 
  #the [ ] is used to grab the value of each successive index element as we proceed through the while loop, yielding each to a block.
  
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
-#  i = 0 
-#  
-#  while i < array.length
-#    i = i + 1 
-#    yield array[i]
-#  end  
-#  
-#  array
-#end  
-  
-
 
  
    
